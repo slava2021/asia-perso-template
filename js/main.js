4852a2467af195739d7469b15d628ad1f5dc19bc
Vyacheslav <asik2006@mail.ru>
2025-04-21 14:57:39 +0300

    Fixing issue top menu

--- js/main.js
+++ js/main.js
@@ -1,27 +1,29 @@
 const button = document.getElementById("top-menu-button");
-const nav = document.getElementById("top-menu-list");
-const sidebar = document.getElementById("sidebar");
+const topMenu = document.getElementById("top-menu")
+const nav = document.getElementById("top-menu-container");
+// const sidebar = document.getElementById("sidebar");
 const language = document.getElementById("language")
 let mql = window.matchMedia("(max-width: 768px)");
 
-mql.addEventListener("change", () =>{
-  console.log(mql)
-    if (mql.matches){
-    language.size = "2"
-    } else {
-      language.size = "1"
-    }
-})
+// mql.addEventListener("change", () =>{
+//   console.log(mql)
+//     if (mql.matches){
+//     language.size = "2"
+//     } else {
+//       language.size = "1"
+//     }
+// })
 
 button.addEventListener("click", () => {
   nav.classList.toggle("show");
-  sidebar.classList.toggle("show")
+  // sidebar.classList.toggle("show")
+  topMenu.classList.toggle("menu-silver")
 
-  if (mql.matches){
-    language.size = "2"
-    } else {
-      language.size = "1"
-    }
+  // if (mql.matches){
+  //   language.size = "2"
+  //   } else {
+  //     language.size = "1"
+  //   }
 });
 
 
