4e1e096930caa886afff8c98ef488781c2ddfb70
Vyacheslav <asik2006@mail.ru>
2025-04-21 12:19:28 +0300

    Add top menu and res 768 and 425

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
 const button = document.getElementById("top-menu-button");
 const nav = document.getElementById("top-menu-list");
+const sidebar = document.getElementById("sidebar");
+const language = document.getElementById("language")
+let mql = window.matchMedia("(max-width: 768px)");
 
-console.log(nav)
+mql.addEventListener("change", () =>{
+  console.log(mql)
+    if (mql.matches){
+    language.size = "2"
+    } else {
+      language.size = "1"
+    }
+})
 
 button.addEventListener("click", () => {
   nav.classList.toggle("show");
+  sidebar.classList.toggle("show")
+
+  if (mql.matches){
+    language.size = "2"
+    } else {
+      language.size = "1"
+    }
 });
+
+
