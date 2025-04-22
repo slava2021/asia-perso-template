16b2ad0e951393d5d9e2698bf4835ed513423203
Vyacheslav <asik2006@mail.ru>
2025-04-22 16:31:57 +0300

    Add buttons and link effects

--- js/main.js
+++ js/main.js
@@ -1,29 +1,24 @@
 const button = document.getElementById("top-menu-button");
 const topMenu = document.getElementById("top-menu")
 const nav = document.getElementById("top-menu-container");
-// const sidebar = document.getElementById("sidebar");
 const language = document.getElementById("language")
 let mql = window.matchMedia("(max-width: 768px)");
+const mail = document.querySelectorAll('.button')
 
-// mql.addEventListener("change", () =>{
-//   console.log(mql)
-//     if (mql.matches){
-//     language.size = "2"
-//     } else {
-//       language.size = "1"
-//     }
-// })
 
 button.addEventListener("click", () => {
   nav.classList.toggle("show");
-  // sidebar.classList.toggle("show")
   topMenu.classList.toggle("menu-silver")
-
-  // if (mql.matches){
-  //   language.size = "2"
-  //   } else {
-  //     language.size = "1"
-  //   }
 });
+
+mail.forEach((item)=>{
+  item.addEventListener("click", () => {
+    // console.log("cliked")
+    const addressee = `procurements@asiaperso.kg`;
+    const subject = `Обратная связь`;
+    const message = `Введите текст Вашего обращения`;
+    location.href = `mailto:${addressee}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(message)}`;
+  })
+})
 
 
